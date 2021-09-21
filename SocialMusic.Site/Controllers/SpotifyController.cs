78314de19eff89cc2029141efcf526ae0c574521
Gabriel Vianna <gabriel1_vianna@hotmail.com>
2021-09-21 16:35:57 -0300

    Add Spotify page

---
+++ SocialMusic.Site/Controllers/SpotifyController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SocialMusic.Site.Controllers
+{
+    public class SpotifyController : Controller
+    {
+        public SpotifyController(IHttpClientFactory httpClientFactory)
+        {
+            postman = httpClientFactory.CreateClient();
+        }
+
+        HttpClient postman;
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ SocialMusic.Site/wwwroot/js/spotify.js
@@ -0,0 +1,4 @@
+﻿var token = window.location.hash.split("=")[1].split("&")[0];
+
+console.log(token)
+
